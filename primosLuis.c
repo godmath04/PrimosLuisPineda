3fee0fdf42e44471b1a39e77d989a8599b4033a5
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-10-04 16:42:04 -0500

    Funcion scanf no permite ingreso de datos

--- primosLuis.c
+++ primosLuis.c
@@ -1,14 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
   // Numero ingresado por el usuario 
-int main(){
-    int valorUsuario;
+int main()
+{
+    int valor;
 
-    printf(" VERIFICAR SI TU NUMERO ES PRIMO O NO ES PRIMO");
-
-    printf("\nIntroduzca un numero entero: ");
-    scanf("%d", &valorUsuario);
-    printf("\nEl numero ingresado es: %d",valorUsuario );
+    printf("Ingrese un numero: ");
+    scanf("%d",&valor);
+    //printf("\nEl numero ingresado es: %d",valorUsuario );
 
     printf("Gracias");
     return 0;
