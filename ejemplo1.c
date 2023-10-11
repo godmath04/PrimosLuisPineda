1955e7f3d607650f2d44955b45882c3e86612d31
Laboratorio <laboratorio@udla-ec.int>
2023-10-11 15:34:19 -0500

    Primeras lineas de codigo

---
+++ ejemplo1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* comentario 1
+       comentario 2  */
+
+
+if (/* condition */)
+{
+    if (/* condition */)
+    {
+        /* code */
+        printf("Hola mundo");
+    }
+
+}
+
+for (size_t i = 0; i < count; i++)
+{
+    /* code */
+}
+
+    return 0;
+}
